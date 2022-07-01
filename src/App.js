946f5121eb5334592e039c0d690a7a9e17570ee5
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-07-02 01:17:08 +0600

    navbar and home banner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Navbar from "./Pages/Shared/Navbar";
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
