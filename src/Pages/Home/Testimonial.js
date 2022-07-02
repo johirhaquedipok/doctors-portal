90a41bf3dc3e456be8a37d7e891e1b7da9a0636d
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-07-03 02:59:30 +0600

    testimonial responsive

--- src/Pages/Home/Testimonial.js
+++ src/Pages/Home/Testimonial.js
@@ -1,8 +1,10 @@
 import quotes from "../../assets/icons/quote.svg";
+import TestimonialCard from "./TestimonialCard/TestimonialCard";
 const Testimonial = () => {
   return (
-    <div className="p-4">
-      <div className="flex items-center justify-between md:items-start ">
+    <div className="p-4 my-16 md:my-28">
+      {/* first part */}
+      <div className="mb-10 md:mb-20 flex items-center justify-between md:items-start ">
         <div>
           <h2 className="text-xl font-bold text-secondary mb-1.5">
             Testimonial
@@ -14,6 +16,12 @@ const Testimonial = () => {
         <div>
           <img src={quotes} alt="" className="w-24 md:w-32 lg:w-40" />
         </div>
+      </div>
+      {/* second part */}
+      <div className="grid md:grid-cols-3 md:gap-7 lg:gap-12">
+        <TestimonialCard />
+        <TestimonialCard />
+        <TestimonialCard />
       </div>
     </div>
   );
---
+++ src/Pages/Home/TestimonialCard/TestimonialCard.js
@@ -0,0 +1,26 @@
+const TestimonialCard = () => {
+  return (
+    <div className="card bg-base-100 shadow-xl p-7">
+      <p className="mb-9">
+        It is a long established fact that by the readable content of a lot
+        layout. The point of using Lorem a more-or-less normal distribu to using
+        Content here, content
+      </p>
+      <div className="flex items-center">
+        {/* avatar */}
+        <div class="avatar pr-3.5 ">
+          <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+            <img src="https://placeimg.com/192/192/people" alt="" />
+          </div>
+        </div>
+        {/* title */}
+        <div>
+          <p className="font-bold">Winson Herry</p>
+          <p>California</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialCard;
