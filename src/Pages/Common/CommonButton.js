3914969e90eea0b8e0af97f9ccdd4a776950bc12
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-07-07 12:17:04 +0600

    A

--- src/Pages/Common/CommonButton.js
+++ src/Pages/Common/CommonButton.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 const CommonButton = ({ address, children }) => {
   return (
-    <Link to={address}>
+    <Link to="/appointment">
       <button
         as={Link}
         className="btn btn-primary bg-gradient-to-r from-secondary to-primary bg-gradient-90 uppercase text-white font-bold"
